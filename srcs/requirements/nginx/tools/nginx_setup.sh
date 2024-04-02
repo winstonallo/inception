a388810b81dbc796501d5423b39d1715059f780e
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-04-02 14:38:27 +0200

    add: nginx dockerfile

---
+++ srcs/requirements/nginx/tools/nginx_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p /etc/nginx/conf.d/
+mkdir -p /etc/ssl/private/
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj "/C=AU/L=VI/O=42/OU=student/CN=abied-ch.42.fr"
+
+nginx -g "daemon off;"
