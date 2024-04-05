5c1b9860883469647c882d0405c07f557f6e7389
winstonallo <arthurbiedchar@gmail.com>
2024-04-05 10:23:01 -0400

    Revert "i have no idea what im doing"
    This reverts commit 67fc108c52f992020073d1c276fb3d800faacfe1.

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,48 +1,28 @@
 #!/bin/sh
 
-set -e
+mysql_install_db
 
-# Initialize the MariaDB data directory
-mysql_install_db > /dev/null
+/etc/init.d/mysql start
 
-# Start the mariadb service in the background
-mysqld_safe --bind-address=0.0.0.0 &
+mysql_secure_installation << _EOF_
 
-# Wait for the MySQL daemon to start and for the socket to become available
-while ! test -S "/run/mysqld/mysqld.sock"; do
-    sleep 1
-done
+Y
+root4life
+root4life
+Y
+n
+Y
+Y
+_EOF_
 
-# Secure the installation and set up the initial database
-mysql -u root <<-EOF
--- Set the root password
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_PWD'; FLUSH PRIVILEGES;" | mysql -uroot
 
--- Remove anonymous users
-DELETE FROM mysql.user WHERE User='';
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DB; GRANT ALL ON $SQL_DB.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD'; FLUSH PRIVILEGES;" | mysql -u root
 
--- Disallow root login remotely
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+mysql -uroot -p$SQL_PWD $SQL_DB < /usr/local/bin/wordpress.sql
 
--- Remove test database and access to it
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+fi
 
--- Reload privilege tables
-FLUSH PRIVILEGES;
-EOF
+/etc/init.d/mysql stop
 
-# Create the WordPress database and user, and grant privileges
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';  # Assuming you introduce MYSQL_USER_PASSWORD for the WordPress user
-GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-# Import the WordPress SQL file
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
-# The script will not reach this point if the database import fails
-# If you want to run mysqld directly and keep it in the foreground (to prevent the container from exiting), use exec:
-exec mysqld --bind-address=0.0.0.0
+exec "$@"
