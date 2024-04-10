7459a3dee2124b42ae78c068551ba6b80fc540c9
winstonallo <arthurbiedchar@gmail.com>
2024-04-10 13:04:46 -0400

    20th times the charm, just understood that i dont have to build the configs from scractc

--- srcs/requirements/mariadb/conf/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mariadb;
-CREATE USER IF NOT EXISTS 'abied-ch'@'localhost' IDENTIFIED BY 'Arthur54600wow';
-GRANT ALL PRIVILEGES ON mariadb.* TO 'abied-ch'@'localhost';
-FLUSH PRIVILEGES;
--- srcs/requirements/wordpress/conf/wp-config.php
+++
@@ -1,6 +0,0 @@
-define('WP_AUTO_UPDATE_CORE', false);
-
-define( 'DB_NAME', 'mariadb' );
-define( 'DB_USER', 'abied-ch' );
-define( 'DB_PASSWORD', 'Arthur54600wow' );
-define( 'DB_HOST', 'localhost' );
