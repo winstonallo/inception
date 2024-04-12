b55ce82c8d0d7432de0ff14cdceabb2399541400
winstonallo <arthurbiedchar@gmail.com>
2024-04-12 03:29:23 -0400

    add: check for database existence before init

--- requirements/mariadb/tools/init.sh
+++ requirements/mariadb/tools/init.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-mysql_install_db
 mysqld_safe &
-sleep 10
 
-cat << EOF > /etc/mysql/init.sql
-CREATE DATABASE $DB_NAME;
-CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
-GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+TIMEOUT=30
+INTERVAL=1
+ELAPSED=0
+
+# Wait for mariadb to start before proceeding
+while ! mysqladmin ping -u root --silent; do
+    sleep $INTERVAL
+    ELAPSED=$(($ELAPSED + $INTERVAL))
+    if [ $ELAPSED -ge $TIMEOUT ]; then
+        echo "MariaDB startup timed out."
+        exit 1
+    fi
+done
+
+# Run MySQL query on database to check existence
+#
+# if not exists:
+#   initialize
+#
+# else:
+#   skip
+DB_EXISTS=$(mysql -u root -sse "SELECT 1 FROM information_schema.schemata WHERE schema_name='$DB_NAME'")
+
+if [ ! "$DB_EXISTS" ]; then
+    cat << EOF > /etc/mysql/init.sql
+    CREATE DATABASE $DB_NAME;
+    CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
+    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
 EOF
 
-mysql -u root < /etc/mysql/init.sql
+    mysql -u root < /etc/mysql/init.sql
+else
+    echo "INFO: Database already installed - skipping initialization."
+fi
 
+# Wait for background processes to finish
 wait
