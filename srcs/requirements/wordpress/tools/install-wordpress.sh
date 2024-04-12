6f9e8ad9b3e6faca202d64cd5c25785454a446db
winstonallo <arthurbiedchar@gmail.com>
2024-04-12 08:41:55 -0400

    add: .env vars for wp install

--- srcs/requirements/wordpress/tools/install-wordpress.sh
+++ srcs/requirements/wordpress/tools/install-wordpress.sh
@@ -5,6 +5,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PWD --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=$DB_ADMIN_USER --admin_password=$DB_ADMIN_PWD --admin_email=admin@admin.com --allow-root
 
 php-fpm7.4 -F
