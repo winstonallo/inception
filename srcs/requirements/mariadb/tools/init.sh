9c05eebd91ea8c514d31d325eb74c0e72fd95046
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-04-15 12:25:49 +0200

    Update init.sh

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,24 +7,6 @@ else
     mysql_install_db --user=$MYSQL_USER --basedir=$DB_BASEDIR --datadir=$DB_DATADIR
 fi
 
-echo "INFO: Temporarily starting MySQL in the background..."
-mysqld_safe --general-log=1 --general-log-file=/var/log/mysql/general.log &
-
-TIMEOUT=30
-INTERVAL=1
-ELAPSED=0
-
-echo "INFO: Waiting for the MySQL service to start..."
-while ! mysqladmin ping -u root --silent; do
-    sleep $INTERVAL
-    ELAPSED=$(($ELAPSED + $INTERVAL))
-    if [ $ELAPSED -ge $TIMEOUT ]; then
-        echo "ERROR: MySQL service startup timed out after ${ELAPSED} seconds."
-        exit 1
-    fi
-done
-echo "INFO: MySQL running."
-
 echo "INFO: Checking for already initialized database.."
 DB_EXISTS=$(mysql -u root -sse "SELECT 1 FROM information_schema.schemata WHERE schema_name='$DB_NAME'")
 if [ ! "$DB_EXISTS" ]; then
@@ -43,6 +25,3 @@ else
     echo "INFO: Database already initialized."
 fi
 
-# Bring MySQL back to the foreground for Docker to manage
-echo "INFO: Pushing MySQL back to the foreground..."
-wait $!
